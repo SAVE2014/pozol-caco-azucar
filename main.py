fbe2cba353c1a72bee85aeb8d76fee0e055a038d
Hector Jimenez <Hectorjimenez@outlook.com>
2014-07-14 22:26:50 -0600

    created admin and app modules in client side and define route handlers

--- main.py
+++ main.py
@@ -3,14 +3,18 @@ import webapp2
 import logging
 from webapp2_extras import routes
 
-from server.item.item_endpoint import ItemEndpoint
+from server.modules.app import AppHandler
+from server.modules.admin import AdminHandler
+from server.item.item_endpont import ItemEndpoint
+from server.seed.seed_entities import SeedEntities
 
-webapp2.WSGIApplication([
+
+app = webapp2.WSGIApplication([
     routes.DomainRoute('admin.sauci-catalog.appspot.com', [
         webapp2.Route('/', handler=AdminHandler, name='admin-home'),
     ]),
     webapp2.Route(r'/api/v1/items<:/?><:(.*)>', handler=ItemEndpoint, name='words-endpoint'),
-    # webapp2.Route('/admin', handler=AdminHandler, name='test'),
-    #webapp2.Route('/seed/<:\w*>', handler=SeedEntities, name='seed'),
+    webapp2.Route('/admin', handler=AdminHandler, name='test'),
+    webapp2.Route('/seed/<:\w*>', handler=SeedEntities, name='seed'),
     webapp2.Route('/', handler=AppHandler, name='home'),
 ], debug=True)
